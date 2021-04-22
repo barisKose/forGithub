74093914995294dfadb1861c038bbf6135e603fb
Barış Köse <bariskose.gazi@outlook.com>
2021-04-23 01:10:54 +0300

    Add project files.

---
+++ forGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
